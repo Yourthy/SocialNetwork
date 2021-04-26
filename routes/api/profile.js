3029ef9726c15755485278fdf88d6080de00dd05
christopher <christopher.t.yourth@gmail.com>
2021-04-25 22:16:23 -0600

    created a profile model and created a current user route in the profile API

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const user = require('../models/User');
+const profile = require('../models/Profile');
 
-//@route    GET api/profile
-//@desc     Test Route
-//@access   Public
-router.get('/', (req, res)=> res.send('Profile Route'));
+//@route    GET api/profile/me
+//@desc     Get current users profile
+//@access   Private
+router.get('/me', async, (req, res)=> {
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+    }catch (err){
+        console.error(err.message);
+        res.status(400).send('Server Error');
+    }
+
+});
 
 
-module.exports= router;
+module.exports = router;
--- routes/models/Profile.js
+++ routes/models/Profile.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
+
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
@@ -14,20 +15,104 @@ const ProfileSchema = new mongoose.Schema({
   location: {
     type: String,
   },
-  status:{
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+ 
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        require: true,
+      },
+      fielofStudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        require: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+
+  social: {
+    youtube: {
       type: String,
-      required: true
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedIn: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
   },
-  skills:{
-      type: [String],
-      required: true
+
+  date: {
+    type: Date,
+    default: Date.now,
   },
-  bio:{
-      type: String
-  },
-  githubusername:{
-      type: String
-  }
 });
 
-module.exports = mongoose.model("user", UserSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
