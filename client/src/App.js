dfc951f91d9670b6839e1951abb30267d86f9f8a
christopher <christopher.t.yourth@gmail.com>
2021-05-09 15:15:49 -0600

    created an alert component and an action call

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
 
 //  Redux
 import { Provider } from 'react-redux';
@@ -20,6 +22,7 @@ const  App = () =>
       <NavBar />
       <Route exact path='/' component={Landing}/>
       <section className="container">
+          <Alert/>
         <Switch>
           <Route exact path='/register' component={Register}/>
           <Route exact path='/login' component={Login}/>
